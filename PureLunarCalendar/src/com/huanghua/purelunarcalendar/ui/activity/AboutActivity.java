9916fd2df81db873aca6f5e59bb3b77ed5e98ee9
huanghua <hhuang@mobvoi.com>
2014-10-29 13:59:45 +0800

    add PureLunarCalendar

---
+++ PureLunarCalendar/src/com/huanghua/purelunarcalendar/ui/activity/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.huanghua.purelunarcalendar.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.huanghua.purelunarcalendar.R;
+
+public class AboutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+
+}
